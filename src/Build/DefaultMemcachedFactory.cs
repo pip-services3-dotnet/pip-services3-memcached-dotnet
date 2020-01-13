1462860052c881166b975944a41ffe6a264b7dd3
Danyil Tretiakov <daniil.tr@hotmail.com>
2020-01-13 21:17:26 +0200

    Added 'pip-services' descriptors

--- src/Build/DefaultMemcachedFactory.cs
+++ src/Build/DefaultMemcachedFactory.cs
@@ -13,17 +13,22 @@ namespace PipServices3.Memcached.Build
     /// <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-memcached-dotnet/master/doc/api/class_pip_services_1_1_memcached_1_1_lock_1_1_memcached_lock.html">MemcachedLock</a>
     public class DefaultMemcachedFactory: Factory
     {
-        public static readonly Descriptor Descriptor = new Descriptor("pip-services3", "factory", "memcached", "default", "1.0");
-        public static readonly Descriptor MemcachedCacheDescriptor = new Descriptor("pip-services3", "cache", "memcached", "*", "1.0");
-        public static readonly Descriptor MemcachedLockDescriptor = new Descriptor("pip-services3", "lock", "memcached", "*", "1.0");
+        public static readonly Descriptor Descriptor = new Descriptor("pip-services", "factory", "memcached", "default", "1.0");
+        public static readonly Descriptor Descriptor3 = new Descriptor("pip-services3", "factory", "memcached", "default", "1.0");
+        public static readonly Descriptor MemcachedCacheDescriptor = new Descriptor("pip-services", "cache", "memcached", "*", "1.0");
+        public static readonly Descriptor MemcachedCache3Descriptor = new Descriptor("pip-services3", "cache", "memcached", "*", "1.0");
+        public static readonly Descriptor MemcachedLockDescriptor = new Descriptor("pip-services", "lock", "memcached", "*", "1.0");
+        public static readonly Descriptor MemcachedLock3Descriptor = new Descriptor("pip-services3", "lock", "memcached", "*", "1.0");
 
         /// <summary>
         /// Create a new instance of the factory.
         /// </summary>
         public DefaultMemcachedFactory()
         {
-            RegisterAsType(DefaultMemcachedFactory.MemcachedCacheDescriptor, typeof(MemcachedCache));
-            RegisterAsType(DefaultMemcachedFactory.MemcachedLockDescriptor, typeof(MemcachedLock));
+            RegisterAsType(MemcachedCacheDescriptor, typeof(MemcachedCache));
+            RegisterAsType(MemcachedCache3Descriptor, typeof(MemcachedCache));
+            RegisterAsType(MemcachedLockDescriptor, typeof(MemcachedLock));
+            RegisterAsType(MemcachedLock3Descriptor, typeof(MemcachedLock));
         }
     }
 }
