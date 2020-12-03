f64dcf3b2c6f10b39a94c602fdb5dbc4e0ffbb7f
banalna <judas.priest999@gmail.com>
2020-12-03 12:05:11 +0300

    fix links in docs

--- src/Cache/MemcachedCache.cs
+++ src/Cache/MemcachedCache.cs
@@ -21,7 +21,7 @@ namespace PipServices3.Memcached.Cache
     /// ### Configuration parameters ###
     /// 
     /// connection(s):
-    /// - discovery_key:         (optional) a key to retrieve the connection from <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a>
+    /// - discovery_key:         (optional) a key to retrieve the connection from <a href="https://pip-services3-dotnet.github.io/pip-services3-components-dotnet/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a>
     /// - host:                  host name or IP address
     /// - port:                  port number
     /// - uri:                   resource URI or connection string with all parameters in it
@@ -41,7 +41,7 @@ namespace PipServices3.Memcached.Cache
     /// 
     /// ### References ###
     /// 
-    /// - *:discovery:*:*:1.0        (optional) <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a> services to resolve connection
+    /// - *:discovery:*:*:1.0        (optional) <a href="https://pip-services3-dotnet.github.io/pip-services3-components-dotnet/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a> services to resolve connection
     /// </summary>
     /// <example>
     /// <code>
