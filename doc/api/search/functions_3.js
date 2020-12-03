41c0ee3ea2ff897e786aae354d56aa05d5dcca52
banalna <judas.priest999@gmail.com>
2020-12-03 12:13:55 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['memcachedcache',['MemcachedCache',['../class_pip_services_1_1_memcached_1_1_cache_1_1_memcached_cache.html#ae82ec95d76dc8772c0d3b2e4bac324d5',1,'PipServices3::Memcached::Cache::MemcachedCache']]]
-];
